ca68142f0df10513f25c892319deb859b5da083a
tiongchuan <tiongchuan.ongtc@gmail.com>
2022-05-30 11:58:25 +0800

    some changes

--- src/config/index.js
+++ src/config/index.js
@@ -8,13 +8,13 @@ let config = {
     baseURL:"https://localhost"
 }
 
-// switch (env.toUpperCase()) {
-//     case "STAGE":
-//         config.baseURL="https://stage.skillsunion.com"
-//         break;
-//     case "PRODUCTION":
-//         config.baseURL="https://skillsunion.com"
-//         break;
-// }
+switch (env.toUpperCase()) {
+    case "STAGE":
+        config.baseURL="https://stage.skillsunion.com"
+        break;
+    case "PRODUCTION":
+        config.baseURL="https://skillsunion.com"
+        break;
+}
 
 export default config;
